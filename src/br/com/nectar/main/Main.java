de519d44dd96f75f13267c7ed6a50163704a6c5f
sanderson <sanderson@172.16.186.1>
2018-02-27 15:39:58 -0300

    Cadastro de usuário e acesso - Final v1.0

--- src/br/com/nectar/main/Main.java
+++ src/br/com/nectar/main/Main.java
@@ -21,11 +21,26 @@ public class Main {
 		String username = "suporte@marconsoft.com.br";
 		String password = "!efacil#rul3z";
 		String name = "Teste Automatizado3 v1.0";
+		//String name = "Teste Automatizado";
 		String email = "teste2@teste.com";
 		String cpf = "88588155990";
 		String group = "CAIXA";
 		String cel = "48999999999";
 		String limitDes = "20";
+						
+		/*
+		 * 
+		 * Invocação do driver Chrome
+		 * 
+		 * */
+		browser = Driver.initChrome();
+		
+		/*
+		 * 
+		 * Testes de Login Inválido Chrome
+		 * 
+		 * */
+		System.out.println("Teste de autenticação inválida está: " + (login.testLoginInvalido(browser) ? "Aprovado" : "Reprovado"));
 		
 		/*
 		 * 
@@ -39,28 +54,34 @@ public class Main {
 		 * Testes de Login Chrome
 		 * 
 		 * */
-		
-		System.out.println("Teste de autenticação inválida está: " + (login.testLoginInvalido(browser) ? "Aprovado" : "Reprovado"));
-		/*
-		 * 
-		 * Invocação do driver Chrome
-		 * 
-		 * */
-		browser = Driver.initChrome();
 		System.out.println("Teste de login está: " + (login.testLoginCorreto(browser) ? "Aprovado" : "Reprovado"));
+		
 		/*
 		 * 
 		 * Invocação do driver Chrome
 		 * 
 		 * */
 		browser = Driver.initChrome();
-		System.out.println("Teste de SQL Infection está: " + (login.testLoginSQLInject(browser) ? "Aprovado" : "Reprovado"));
+		
+		/*
+		 * 
+		 * Testes de Injeção de SQL Chrome
+		 * 
+		 * */
+		System.out.println("Teste de SQL Injection está: " + (login.testLoginSQLInject(browser) ? "Aprovado" : "Reprovado"));
+		
 		/*
 		 * 
 		 * Invocação do driver Chrome
 		 * 
 		 * */
 		browser = Driver.initChrome();
+		
+		/*
+		 * 
+		 * Testes de ataque de força bruta no Chrome
+		 * 
+		 * */
 		System.out.println("Teste de Ataque de força Bruta .\nAGUARDE!");
 		System.out.println("Teste de Ataque de força Bruta está: " + (login.testLoginForcaBruta(browser) ? "Aprovado" : "Reprovado"));
 		
@@ -69,9 +90,14 @@ public class Main {
 		 * Invocação do driver Chrome
 		 * 
 		 * */
-		
 		browser = Driver.initChrome();
-		System.out.println("O cadastro está: " + (user.testUserForRegistry( 
+		
+		/*
+		 * 
+		 * Testes de validação de email no cadastro de usuário no Chrome
+		 * 
+		 * */
+		System.out.println("A validação do email do cadastro de usuário está: " + (user.testUserFormRegistryEmailValidade( 
 				browser,
 				URL,
 				username, 
@@ -82,60 +108,127 @@ public class Main {
 				group,
 				cel, 
 				limitDes)? "Aprovado" : "Reprovado"));
+		
+		/*
+		 * 
+		 * Invocação do driver Chrome
+		 * 
+		 * */
+		
+		browser = Driver.initChrome();
+		
+		/*
+		 * 
+		 * Testes do cadastro de usuário no Chrome
+		 * 
+		 * */
+		System.out.println("O cadastro está: " + (user.testUserFormRegistry( 
+				browser,
+				URL,
+				username, 
+				password, 
+				name, 
+				email, 
+				cpf,
+				group,
+				cel, 
+				limitDes)? "Aprovado" : "Reprovado"));
+		
+		/*
+		 * 
+		 * Invocação do driver Chrome
+		 * 
+		 * */
+		
+		browser = Driver.initChrome();
+		
+		/*
+		 * 
+		 * Testes do localizador de usuário no Chrome
+		 * 
+		 * */
+		System.out.println("A procura do cadastro de usuário está: " + (user.testSearchUserFormRegistry( 
+				browser,
+				URL,
+				username, 
+				password, 
+				name, 
+				email, 
+				cpf,
+				group,
+				cel, 
+				limitDes) ? "Aprovado" : "Reprovado"));
 				
 		/*
 		 * 
-		 * Invocação do driver do Firefox
+		 * Invocação do driver Firefox
 		 * 
 		 * */
-		browser = Driver.initFirefox(); 
-
+		browser = Driver.initFirefox();
+		
+		/*
+		 * 
+		 * Testes de Login Inválido Firefox
+		 * 
+		 * */
+		System.out.println("Teste de autenticação inválida está: " + (login.testLoginInvalido(browser) ? "Aprovado" : "Reprovado"));
+		
+		/*
+		 * 
+		 * Invocação do driver Firefox
+		 * 
+		 * */
+		browser = Driver.initFirefox();
+		
 		/*
 		 * 
 		 * Testes de Login Firefox
 		 * 
 		 * */
-		
-		System.out.println("Teste de autenticação inválida está: " + (login.testLoginInvalido(browser) ? "Aprovado" : "Reprovado"));
-		
-		/*
-		 * 
-		 * Invocação do driver do Firefox
-		 * 
-		 * */
-		browser = Driver.initFirefox(); 
 		System.out.println("Teste de login está: " + (login.testLoginCorreto(browser) ? "Aprovado" : "Reprovado"));
 		
 		/*
 		 * 
-		 * Invocação do driver do Firefox
+		 * Invocação do driver Firefox
 		 * 
 		 * */
-		browser = Driver.initFirefox(); 
-		System.out.println("Teste de SQL Infection está: " + (login.testLoginSQLInject(browser) ? "Aprovado" : "Reprovado"));
-
+		browser = Driver.initFirefox();
+		
 		/*
 		 * 
-		 * Invocação do driver do Firefox
+		 * Testes de Injeção de SQL Firefox
 		 * 
 		 * */
-		System.out.println("Teste de Ataque de força Bruta.\nAGUARDE!");
-
+		System.out.println("Teste de SQL Injection está: " + (login.testLoginSQLInject(browser) ? "Aprovado" : "Reprovado"));
+		
 		/*
 		 * 
-		 * Invocação do driver do Firefox
+		 * Invocação do driver Firefox
 		 * 
 		 * */
-		browser = Driver.initFirefox(); 
+		browser = Driver.initFirefox();
+		
+		/*
+		 * 
+		 * Testes de ataque de força bruta no Firefox
+		 * 
+		 * */
+		System.out.println("Teste de Ataque de força Bruta .\nAGUARDE!");
 		System.out.println("Teste de Ataque de força Bruta está: " + (login.testLoginForcaBruta(browser) ? "Aprovado" : "Reprovado"));
-
+		
 		/*
 		 * 
-		 * Invocação do driver do Firefox
+		 * Invocação do driver Firefox
 		 * 
 		 * */
-		browser = Driver.initFirefox(); 
-		System.out.println("O cadastro está: " + (user.testUserForRegistry( 
+		browser = Driver.initFirefox();
+		
+		/*
+		 * 
+		 * Testes de validação de email no cadastro de usuário no Firefox
+		 * 
+		 * */
+		System.out.println("A validação do email do cadastro de usuário está: " + (user.testUserFormRegistryEmailValidade( 
 				browser,
 				URL,
 				username, 
@@ -146,5 +239,56 @@ public class Main {
 				group,
 				cel, 
 				limitDes)? "Aprovado" : "Reprovado"));
+		
+		/*
+		 * 
+		 * Invocação do driver Firefox
+		 * 
+		 * */
+		
+		browser = Driver.initFirefox();
+		
+		/*
+		 * 
+		 * Testes do cadastro de usuário no Firefox
+		 * 
+		 * */
+		System.out.println("O cadastro está: " + (user.testUserFormRegistry( 
+				browser,
+				URL,
+				username, 
+				password, 
+				name, 
+				email, 
+				cpf,
+				group,
+				cel, 
+				limitDes)? "Aprovado" : "Reprovado"));
+		
+		/*
+		 * 
+		 * Invocação do driver Firefox
+		 * 
+		 * */
+		
+		browser = Driver.initFirefox();
+		
+		/*
+		 * 
+		 * Testes do localizador de usuário no Firefox
+		 * 
+		 * */
+		System.out.println("A procura do cadastro de usuário está: " + (user.testSearchUserFormRegistry( 
+				browser,
+				URL,
+				username, 
+				password, 
+				name, 
+				email, 
+				cpf,
+				group,
+				cel, 
+				limitDes) ? "Aprovado" : "Reprovado"));
+
 	}
 }
